cd9df8ee166bb121a3b1edb8fc55bb11fdd8313a
AmoabaKelvin <kel.amoaba@gmail.com>
2024-08-22 14:49:54 +0000

    refactor: Update header component with Clerk user button

--- src/app/(main)/_components/header.tsx
+++ src/app/(main)/_components/header.tsx
@@ -1,3 +1,4 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,12 +7,12 @@ import { APP_TITLE } from "@/lib/constants";
 export const Header = async () => {
   return (
     <header className="sticky top-0 z-10 border-b bg-background/80 p-0">
-      <div className="mx-auto flex max-w-6xl items-center gap-2 px-2 py-2 lg:px-4">
+      <div className="mx-auto flex max-w-6xl items-center justify-between gap-2 px-2 py-2 lg:px-4">
         <Link className="flex items-center justify-center text-xl font-medium" href="/">
-          <Image src="logoPurple.svg" alt="Logo" width={40} height={40} />
-          {APP_TITLE} Dashboard
+          <Image src="/logo/logo.png" alt="Logo" width={40} height={40} />
+          {APP_TITLE}
         </Link>
-        {/* place clerk user dropdown here */}
+        <UserButton />
       </div>
     </header>
   );
